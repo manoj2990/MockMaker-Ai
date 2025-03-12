d964d3232740e7a2f4377f3dfe983c25e8ea82cd
manoj2990 <manojmeena2990@gmail.com>
2025-03-12 19:05:48 +0530

    remove dbURL from drizzle

--- drizzle.config.js
+++ drizzle.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   schema: './utils/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://neonDB_owner:mp7HLROc1FQk@ep-twilight-cloud-a8xph1b5.eastus2.azure.neon.tech/neonDB?sslmode=require',
+    url: NEXT_PUBLIC_DRIZZLE_DB_URL,
   },
 });
